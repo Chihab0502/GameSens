ae53bc46a049becd898b07a48fec9454d2da5348
Chihab0502 <118019231+Chihab0502@users.noreply.github.com>
2024-05-16 16:04:15 +0200

    Footer sa arrive inshallah

---
+++ js/footer.js
@@ -0,0 +1,5 @@
+// footer.js
+document.getElementById('footerToggle').addEventListener('click', function() {
+    var footerContent = document.getElementById('footerContent');
+    footerContent.style.display = footerContent.style.display === 'block' ? 'none' : 'block';
+});
