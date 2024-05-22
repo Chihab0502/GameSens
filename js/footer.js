5da06ae2529c9a153139d71e2f59ca7c55a0467b
Chihab0502 <118019231+Chihab0502@users.noreply.github.com>
2024-05-22 17:11:40 +0200

    animation

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,18 @@
 // footer.js
 document.getElementById('footerToggle').addEventListener('click', function() {
     var footerContent = document.getElementById('footerContent');
-    footerContent.style.display = footerContent.style.display === 'block' ? 'none' : 'block';
+    if (footerContent.style.display === 'block' && !footerContent.classList.contains('closing')) {
+        footerContent.style.display = 'none'; // Masque le footer seulement si l'animation de fermeture n'est pas en cours
+    } else {
+        footerContent.style.display = 'block'; // Affiche le footer
+    }
+});
+
+document.querySelector('.close-btn').addEventListener('click', function() {
+    var footerContent = document.getElementById('footerContent');
+    footerContent.classList.add('closing');
+    setTimeout(function() {
+        footerContent.style.display = 'none';
+        footerContent.classList.remove('closing'); // Nettoyage de la classe après la fermeture
+    }, 300); // La durée doit correspondre à celle de l'animation CSS
 });
