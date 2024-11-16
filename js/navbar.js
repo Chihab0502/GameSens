5698ea7a7f855b061e123a018d56917eec85ca4b
Chihab0502 <118019231+Chihab0502@users.noreply.github.com>
2024-11-16 18:27:31 +0100

    enlver panier

--- js/navbar.js
+++ js/navbar.js
@@ -1,20 +1,3 @@
-var cartToggle = document.getElementById('cartToggle');
-var cartContent = document.getElementById('cartContent');
-var backdrop = document.getElementById('backdrop');
-
-// Affiche le panier et le fond flouté lorsque la souris entre sur le bouton
-cartToggle.addEventListener('mouseenter', function() {
-    cartContent.style.display = 'flex'; // Assure-toi que le display est en flex pour respecter tes styles
-    cartContent.style.right = '0'; // Déplace le panier pour qu'il soit visible
-    backdrop.style.display = 'block'; // Affiche le fond flouté
-});
-
-// Masque le panier et le fond flouté lorsque la souris sort du panier
-cartContent.addEventListener('mouseleave', function() {
-    cartContent.style.right = '-650px'; // Remet le panier à l'extérieur de la vue
-    backdrop.style.display = 'none'; // Cache le fond flouté
-});
-
 // Sélection des éléments
 const hamburgerButton = document.querySelector('.navbar-burger');
 const menuOverlay = document.getElementById('menuOverlay');
