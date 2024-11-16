31a6aea7eb0342d4080586024707908546342eea
Chihab0502 <118019231+Chihab0502@users.noreply.github.com>
2024-11-16 18:08:44 +0100

    Ajout bouton hamburger + menu déroulant

--- js/navbar.js
+++ js/navbar.js
@@ -14,3 +14,18 @@ cartContent.addEventListener('mouseleave', function() {
     cartContent.style.right = '-650px'; // Remet le panier à l'extérieur de la vue
     backdrop.style.display = 'none'; // Cache le fond flouté
 });
+
+// Sélection des éléments
+const hamburgerButton = document.querySelector('.navbar-burger');
+const menuOverlay = document.getElementById('menuOverlay');
+
+// Événement pour afficher/masquer le menu
+hamburgerButton.addEventListener('click', () => {
+    if (menuOverlay.style.display === 'none') {
+        menuOverlay.style.display = 'block';
+        setTimeout(() => menuOverlay.classList.add('show'), 10); // Transition visible
+    } else {
+        menuOverlay.classList.remove('show');
+        setTimeout(() => menuOverlay.style.display = 'none', 300); // Attendre la fin de la transition
+    }
+});
